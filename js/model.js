2cbeb1ad06809b3d5cfbc0185ca04f5639e3714e
Game Building Tools <gamdbuildingtools@gmail.com>
2017-08-13 19:28:32 -0400

    Add model, view, and cleaned up folder structure

---
+++ js/model.js
@@ -0,0 +1,26 @@
+var myLocations = [
+  {
+    title: "Rathskellar",
+    address: "401 E Michigan St, Indianapolis, IN 46204"
+  },
+  {
+    title: "Bru Burger Bar",
+    address: "410 Massachusetts Ave, Indianapolis, IN 46204",
+  },
+  {
+    title: "The Tap",
+    address: "306 N Delaware St, Indianapolis, IN 46204",
+  },
+  {
+    title: "Bazbeaux",
+    address: "329 Massachusetts Ave, Indianapolis, IN 46204"
+  },
+  {
+    title: "Roberts Park Methodist Church",
+    address: "401 N Delaware St, Indianapolis, IN 46204"
+  },
+  {
+    title: "Chatterbox Jazz Club",
+    address: "435 Massachusetts Ave, Indianapolis, IN 46204"
+  }
+];
