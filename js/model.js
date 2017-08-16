9f4e406e5977733de3c53786871d7446110e0ae5
Game Building Tools <gamdbuildingtools@gmail.com>
2017-08-15 21:02:49 -0400

    Add a filter option and slight layout changes

--- js/model.js
+++ js/model.js
@@ -5,29 +5,36 @@ locations.startingPlace = {
   lng: -86.151869
 }
 
+// This information would preferably come from a server.
 locations.myPlaces = [
   {
     title: "Rathskellar",
-    address: "401 E Michigan St, Indianapolis, IN 46204"
+    address: "401 E Michigan St, Indianapolis, IN 46204",
+    latlng: {lat: 39.773932, lng: -86.150062}
   },
   {
     title: "Bru Burger Bar",
     address: "410 Massachusetts Ave, Indianapolis, IN 46204",
+    latlng: {lat: 39.772994, lng: -86.151852}
   },
   {
     title: "The Tap",
     address: "306 N Delaware St, Indianapolis, IN 46204",
+    latlng: {lat: 39.771924, lng: -86.154258}
   },
   {
     title: "Bazbeaux",
-    address: "329 Massachusetts Ave, Indianapolis, IN 46204"
+    address: "329 Massachusetts Ave, Indianapolis, IN 46204",
+    latlng: {lat: 39.771797, lng: -86.153525}
   },
   {
     title: "Roberts Park Methodist Church",
-    address: "401 N Delaware St, Indianapolis, IN 46204"
+    address: "401 N Delaware St, Indianapolis, IN 46204",
+    latlng: {lat: 39.772951, lng: -86.154218}
   },
   {
     title: "Chatterbox Jazz Club",
-    address: "435 Massachusetts Ave, Indianapolis, IN 46204"
+    address: "435 Massachusetts Ave, Indianapolis, IN 46204",
+    latlng: {lat: 39.773406, lng: -86.151295}
   }
 ];
--- js/view.js
+++ js/view.js
@@ -1,10 +1,93 @@
+var map;
+var locationsList = ko.observableArray([]);
+var markers = [];
+
 function initMap() {
-  var map = new google.maps.Map(document.getElementById('map'), {
+
+  // Create a map and zoom in on Massachusetts Avenue
+  map = new google.maps.Map(document.getElementById('map'), {
     zoom: 17,
     center: locations.startingPlace
   });
-  var marker = new google.maps.Marker({
-    position: locations.startingPlace,
-    map: map
+
+  // Apply Bindings along with the map init
+  ko.applyBindings(new DisplayLocations());
+}
+
+function populateInfoWindow(marker, infowindow) {
+  if(infowindow.marker != marker) {
+    infowindow.marker = marker;
+    infowindow.setContent('<div>' + marker.title + '<br />' + marker.address + '</div>');
+    infowindow.open(map, marker);
+
+    infowindow.addListener('closeclick',function(){
+      infowindow.marker = null;
+    });
+  }
+}
+
+function ViewModel(){
+  /*var self = this;
+  this.filter = ko.observable();
+  var myLocations = [];
+
+  locations.myPlaces.forEach(function(location, index) {
+    myLocations.push(location);
   });
+
+  this.places = ko.observableArray(myLocations);
+
+  this.myPlaces = ko.computed(function(){
+    return this.places().filter(function(place){
+      if(!self.filter() || place.title.toLowerCase().indexOf(self.filter().toLowerCase()) !== -1) {
+        console.log( place.title.toLowerCase() );
+        return place;
+      }
+    });
+  }, this);*/
+
+}
+
+//ko.applyBindings(new ViewModel());
+
+function DisplayLocations() {
+
+  var self = this;
+  this.filter = ko.observable();
+  this.locations = locations.myPlaces;
+  this.places = ko.observableArray(this.locations);
+
+  var largeInfowindow = new google.maps.InfoWindow();
+  var bounds = new google.maps.LatLngBounds();
+
+  this.locations.forEach(function(location, index) {
+
+    // Create a marker for each location
+    var marker = new google.maps.Marker({
+      position: location.latlng,
+      map: map,
+      title: location.title,
+      address: location.address,
+      animation: google.maps.Animation.DROP,
+    });
+
+    // Push the marker to my markers array
+    markers.push(marker);
+
+    // Add a click event to open the marker
+    marker.addListener('click', function() {
+      populateInfoWindow(this, largeInfowindow);
+    });
+  });
+
+  // Create the filter
+  this.myPlace = ko.computed(function(){
+    return this.places().filter(function(place){
+      if(!self.filter() || place.title.toLowerCase().indexOf(self.filter().toLowerCase()) !== -1) {
+        //console.log( place.title.toLowerCase() );
+        return place;
+      }
+    });
+  }, this);
+
 }
